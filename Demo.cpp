938b10d17f5bd0bef5fd71bd819c3e7f19e3fd97
AviShtarkberg <143038302+AviShtarkberg@users.noreply.github.com>
2024-05-17 01:44:11 +0300

    Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -1,100 +1,146 @@
 // id:322530080 mail:avishb1213@gmail.com
+
+// demo class to run the code.
 #include "Algorithms.hpp"
 #include <iostream>
 #include <stdexcept>
 #include <vector>
-using namespace std;
-using ariel::Algorithms;
+using namespace ariel;
+
+// will print 3 new lines
+void print_space()
+{
+    cout << endl;
+    cout << endl;
+    cout << endl;
+}
 int main()
 {
-    ariel::Graph g;
-    // 3x3 matrix that represents a connected graph.
-    vector<vector<int>> graph = {
-        {0, 1, 0},
-        {1, 0, 1},
-        {0, 1, 0}};
-    g.loadGraph(graph, false); // Load the graph to the object.
-    cout << "graph1 print:" << endl;
-    g.printGraph();
-    cout << endl;
-
-    cout << "algorithems on graph1:     " << endl;
-    cout << "isConnected:";
-    cout << Algorithms::isConnected(g) << endl; // Should print: "1" (true).
-    cout << "shortestPath:";
-    cout << Algorithms::shortestPath(g, 0, 2) << endl; // Should print: 0->1->2.
-    cout << "isContainsCycle:";
-    cout << Algorithms::isContainsCycle(g) << endl; // Should print: "0" (false).
-    cout << "isBipartite:";
-    cout << Algorithms::isBipartite(g) << endl; // Should print: "The graph is bipartite: A={0, 2}, B={1}."
-
-    cout << endl;
-    cout << endl;
-    cout << endl;
-
+    print_space();
+    Graph g1;
     // 5x5 matrix that represents a non-connected graph with a cycle.
-    vector<vector<int>> graph2 = {
+    vector<vector<int>> graph1 = {
         {0, 1, 1, 0, 0},
         {1, 0, 1, 0, 0},
         {1, 1, 0, 1, 0},
         {0, 0, 1, 0, 0},
         {0, 0, 0, 0, 0}};
 
-    g.loadGraph(graph2, false); // Load the graph to the object.
-    cout << "graph2 print:" << endl;
-    g.printGraph();
+    g1.loadGraph(graph1, true);
+    cout << g1; // usege of cout operator.
     cout << endl;
-    cout << "algorithems on graph2:     " << endl;
+    cout << "algorithems on graph1:     " << endl;
     cout << "isConnected:";
-    cout << Algorithms::isConnected(g) << endl; // Should print: "0" (false).
+    cout << Algorithms::isConnected(g1) << endl; // Should print: "0" (false).
     cout << "shortestPath:";
-    cout << Algorithms::shortestPath(g, 0, 4) << endl; // Should print: "-1" (there is no path between 0 and 4).
+    cout << Algorithms::shortestPath(g1, 0, 4) << endl; // Should print: "-1" (there is no path between 0 and 4).
     cout << "isContainsCycle:";
-    cout << Algorithms::isContainsCycle(g) << endl; // Should print: "The cycle is: 0->1->2->0".
+    cout << Algorithms::isContainsCycle(g1) << endl; // Should print: "The cycle is: 0->1->2->0".
     cout << "isBipartite:";
-    cout << Algorithms::isBipartite(g) << endl; // Should print: "0" (false).
+    cout << Algorithms::isBipartite(g1) << endl; // Should print: "0" (false).
 
-    cout << endl;
-    cout << endl;
-    cout << endl;
+    print_space();
 
-    // 5x5 matrix that reprsents a connected weighted graph.
-    vector<vector<int>> graph3 = {
-        {0, 1, 2, 0, 0},
-        {1, 0, 3, 0, 0},
-        {2, 3, 0, 4, 0},
-        {0, 0, 4, 0, 5},
-        {0, 0, 0, 5, 0}};
-    g.loadGraph(graph3, false); // Load the graph to the object.
-    cout << "graph3 print:" << endl;
-    g.printGraph();
-    cout << endl;
-    cout << "algorithems on graph3:     " << endl;
-    cout << "isConnected:";
-    cout << Algorithms::isConnected(g) << endl; // Should print: "1" (true).
-    cout << "shortestPath:";
-    cout << Algorithms::shortestPath(g, 0, 4) << endl; // Should print: 0->2->3->4.
-    cout << "isContainsCycle:";
-    cout << Algorithms::isContainsCycle(g) << endl; // Should print: "0->1->2->0" (false).
-    cout << "isBipartite:";
-    cout << Algorithms::isBipartite(g) << endl; // Should print: "The graph is bipartite: 0."
-    cout << endl;
-    cout << endl;
-    cout << endl;
-
+    Graph nc;
     cout << "graph with negative cycle:";
     vector<vector<int>> negative_cycle =
         {{0, 0, 0, 0},
          {0, 0, 0, 0},
          {0, 0, 0, 99},
          {0, 0, -100, 0}};
-    g.loadGraph(negative_cycle, true);
+    nc.loadGraph(negative_cycle, true);
     cout << "graph3 print:" << endl;
-    g.printGraph();
+    nc.printGraph();
     cout << "the negative cycle:";
-    cout << Algorithms::negativeCycle(g);
-    cout << endl;
-    cout << endl;
-    cout << endl;
+    cout << Algorithms::negativeCycle(nc);
+    print_space();
+
+
+    // 5x5 matrix that reprsents a connected weighted graph.
+    Graph g2;
+    vector<vector<int>> graph2 = {
+        {0, 1, 2, 0, 0},
+        {1, 0, 3, 0, 0},
+        {2, 3, 0, 4, 0},
+        {0, 0, 4, 0, 5},
+        {0, 0, 0, 5, 0}};
+    g2.loadGraph(graph2, true);
+    cout << g2; // usege of cout operator.
+    print_space();
+    Graph g3;
+    cout << "operators usege:" << endl;
+
+    g3 = g1 + g2;
+    cout << "+ operator: graph1 + graph2" << endl;
+    g3.print_adjacencyMatrix();
+    print_space();
+
+    g3 = g1 - g2;
+    cout << "- operator: graph1 - graph2" << endl;
+    g3.print_adjacencyMatrix();
+    print_space();
+
+    g3 = ++g1;
+    cout << "++ operator: ++graph1" << endl;
+    g3.print_adjacencyMatrix();
+    print_space();
+
+    g1 += g2;
+    cout << "+= operator: graph1 += g2" << endl;
+    g1.print_adjacencyMatrix();
+    print_space();
+
+    g1 -= g2;
+    cout << "-= operator: graph1 -= g2" << endl;
+    g1.print_adjacencyMatrix();
+    print_space();
+
+    g3 = --g1;
+    cout << "-- operator: --graph1" << endl;
+    g3.print_adjacencyMatrix();
+    print_space();
+
+    g3 = +g1;
+    cout << "unary + operator: +graph1" << endl;
+    g3.print_adjacencyMatrix();
+    print_space();
+
+    g3 = -g1;
+    cout << "unary - operator: -graph1" << endl;
+    g3.print_adjacencyMatrix();
+    print_space();
+
+    g3 = g1 * 3;
+    cout << "*(int) operator: graph1*3" << endl;
+    g3.print_adjacencyMatrix();
+    print_space();
+
+    g3 = g1 * g2;
+    cout << "* operator: graph1 * graph2" << endl;
+    g3.print_adjacencyMatrix();
+    print_space();
+
+    cout << "graph1 == graph2 ? ";
+    cout << (graph1 == graph2);
+    print_space();
+
+    cout << "graph1 != graph2 ? ";
+    cout << (graph1 != graph2);
+    print_space();
+
+    cout << "graph1 < graph2 ? ";
+    cout << (graph1 < graph2);
+    print_space();
+
+    cout << "graph1 <= graph2? ";
+    cout << (graph1 <= graph2);
+    print_space();
+
+    cout << "graph1 > graph2 ? ";
+    cout << (graph1 > graph2);
+    print_space();
+
+    cout << "graph1 >= graph2 ? ";
+    cout << (graph1 >= graph2) << endl;
     return 0;
 }
